448a6289f3b66cd2388f8ad34b50c850e88be4ac
ayrbox <srdx7@hotmail.com>
2020-02-27 12:32:12 +0000

    Setting up project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Oroka Component Library
+
+This is trial of [post in medium](!https://medium.com/@tomasz.fiechowski/building-a-react-components-library-2e116df187b5).
+
+Note: Instead used typescript to write components and Docz is used to showcase the library.
---
+++ src/components/buttonStyles.tsx
@@ -0,0 +1,26 @@
+import { css } from '@emotion/core';
+
+export const normal = css({
+  padding: '10px 20px',
+  fontSize: '16px',
+});
+
+export const small = css({
+  padding: '5px 10px',
+  fontSize: '14px',
+});
+
+
+export const big = css({
+  padding: '20px 30px',
+  fontSize: 18,
+});
+
+
+export const sizeStyles = {
+  normal,
+  small,
+  big,
+};
+
+
